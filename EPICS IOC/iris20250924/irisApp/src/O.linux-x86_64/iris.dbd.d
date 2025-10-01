95e056a3ffaf70b79fec412b20584ff2a3e4d9a3
Massimo Martello <mgmartello99@gmail.com>
2025-10-01 14:14:10 +0200

    Add files via upload

---
+++ EPICS IOC/iris20250924/irisApp/src/O.linux-x86_64/iris.dbd.d
@@ -0,0 +1,152 @@
+../O.Common/iris.dbd: /home/spes/EPICS/base-7.0.8.1/dbd/base.dbd \
+    /home/spes/EPICS/base-7.0.8.1/dbd/menuGlobal.dbd \
+    /home/spes/EPICS/base-7.0.8.1/dbd/menuAlarmSevr.dbd \
+    /home/spes/EPICS/base-7.0.8.1/dbd/menuAlarmStat.dbd \
+    /home/spes/EPICS/base-7.0.8.1/dbd/menuFtype.dbd \
+    /home/spes/EPICS/base-7.0.8.1/dbd/menuIvoa.dbd \
+    /home/spes/EPICS/base-7.0.8.1/dbd/menuOmsl.dbd \
+    /home/spes/EPICS/base-7.0.8.1/dbd/menuPini.dbd \
+    /home/spes/EPICS/base-7.0.8.1/dbd/menuPost.dbd \
+    /home/spes/EPICS/base-7.0.8.1/dbd/menuPriority.dbd \
+    /home/spes/EPICS/base-7.0.8.1/dbd/menuYesNo.dbd \
+    /home/spes/EPICS/base-7.0.8.1/dbd/menuSimm.dbd \
+    /home/spes/EPICS/base-7.0.8.1/dbd/menuConvert.dbd \
+    /home/spes/EPICS/base-7.0.8.1/dbd/menuScan.dbd \
+    /home/spes/EPICS/base-7.0.8.1/dbd/stdRecords.dbd \
+    /home/spes/EPICS/base-7.0.8.1/dbd/aaiRecord.dbd \
+    /home/spes/EPICS/base-7.0.8.1/dbd/dbCommon.dbd \
+    /home/spes/EPICS/base-7.0.8.1/dbd/aaoRecord.dbd \
+    /home/spes/EPICS/base-7.0.8.1/dbd/aiRecord.dbd \
+    /home/spes/EPICS/base-7.0.8.1/dbd/aoRecord.dbd \
+    /home/spes/EPICS/base-7.0.8.1/dbd/aSubRecord.dbd \
+    /home/spes/EPICS/base-7.0.8.1/dbd/biRecord.dbd \
+    /home/spes/EPICS/base-7.0.8.1/dbd/boRecord.dbd \
+    /home/spes/EPICS/base-7.0.8.1/dbd/calcRecord.dbd \
+    /home/spes/EPICS/base-7.0.8.1/dbd/calcoutRecord.dbd \
+    /home/spes/EPICS/base-7.0.8.1/dbd/compressRecord.dbd \
+    /home/spes/EPICS/base-7.0.8.1/dbd/dfanoutRecord.dbd \
+    /home/spes/EPICS/base-7.0.8.1/dbd/eventRecord.dbd \
+    /home/spes/EPICS/base-7.0.8.1/dbd/fanoutRecord.dbd \
+    /home/spes/EPICS/base-7.0.8.1/dbd/histogramRecord.dbd \
+    /home/spes/EPICS/base-7.0.8.1/dbd/int64inRecord.dbd \
+    /home/spes/EPICS/base-7.0.8.1/dbd/int64outRecord.dbd \
+    /home/spes/EPICS/base-7.0.8.1/dbd/longinRecord.dbd \
+    /home/spes/EPICS/base-7.0.8.1/dbd/longoutRecord.dbd \
+    /home/spes/EPICS/base-7.0.8.1/dbd/lsiRecord.dbd \
+    /home/spes/EPICS/base-7.0.8.1/dbd/lsoRecord.dbd \
+    /home/spes/EPICS/base-7.0.8.1/dbd/mbbiRecord.dbd \
+    /home/spes/EPICS/base-7.0.8.1/dbd/mbbiDirectRecord.dbd \
+    /home/spes/EPICS/base-7.0.8.1/dbd/mbboRecord.dbd \
+    /home/spes/EPICS/base-7.0.8.1/dbd/mbboDirectRecord.dbd \
+    /home/spes/EPICS/base-7.0.8.1/dbd/permissiveRecord.dbd \
+    /home/spes/EPICS/base-7.0.8.1/dbd/printfRecord.dbd \
+    /home/spes/EPICS/base-7.0.8.1/dbd/selRecord.dbd \
+    /home/spes/EPICS/base-7.0.8.1/dbd/seqRecord.dbd \
+    /home/spes/EPICS/base-7.0.8.1/dbd/stateRecord.dbd \
+    /home/spes/EPICS/base-7.0.8.1/dbd/stringinRecord.dbd \
+    /home/spes/EPICS/base-7.0.8.1/dbd/stringoutRecord.dbd \
+    /home/spes/EPICS/base-7.0.8.1/dbd/subRecord.dbd \
+    /home/spes/EPICS/base-7.0.8.1/dbd/subArrayRecord.dbd \
+    /home/spes/EPICS/base-7.0.8.1/dbd/waveformRecord.dbd \
+    /home/spes/EPICS/base-7.0.8.1/dbd/filters.dbd \
+    /home/spes/EPICS/base-7.0.8.1/dbd/links.dbd \
+    /home/spes/EPICS/base-7.0.8.1/dbd/devSoft.dbd \
+    /home/spes/EPICS/base-7.0.8.1/dbd/asSub.dbd \
+    /home/spes/EPICS/base-7.0.8.1/dbd/dbCore.dbd \
+    /home/spes/EPICS/base-7.0.8.1/dbd/rsrv.dbd \
+    /home/spes/EPICS/base-7.0.8.1/support/asyn/dbd/asyn.dbd \
+    /home/spes/EPICS/base-7.0.8.1/support/asyn/dbd/asynRecord.dbd \
+    /home/spes/EPICS/base-7.0.8.1/support/asyn/dbd/devEpics.dbd \
+    /home/spes/EPICS/base-7.0.8.1/support/asyn/dbd/devAsynOctet.dbd \
+    /home/spes/EPICS/base-7.0.8.1/support/asyn/dbd/devAsynInt32.dbd \
+    /home/spes/EPICS/base-7.0.8.1/support/asyn/dbd/devAsynXXXArray.dbd \
+    /home/spes/EPICS/base-7.0.8.1/support/asyn/dbd/devAsynInt32TimeSeries.dbd \
+    /home/spes/EPICS/base-7.0.8.1/support/asyn/dbd/devAsynUInt32Digital.dbd \
+    /home/spes/EPICS/base-7.0.8.1/support/asyn/dbd/devAsynFloat64.dbd \
+    /home/spes/EPICS/base-7.0.8.1/support/asyn/dbd/devAsynFloat64TimeSeries.dbd \
+    /home/spes/EPICS/base-7.0.8.1/support/asyn/dbd/devAsynRecord.dbd \
+    /home/spes/EPICS/base-7.0.8.1/support/asyn/dbd/devAsynOctetLs.dbd \
+    /home/spes/EPICS/base-7.0.8.1/support/asyn/dbd/devAsynInt64.dbd \
+    /home/spes/EPICS/base-7.0.8.1/support/asyn/dbd/devAsynInt64Array.dbd \
+    /home/spes/EPICS/base-7.0.8.1/support/asyn/dbd/devAsynInt64TimeSeries.dbd \
+    /home/spes/EPICS/base-7.0.8.1/support/asyn/dbd/devAsynInt64Misc.dbd \
+    /home/spes/EPICS/base-7.0.8.1/support/asyn/dbd/drvAsynIPPort.dbd \
+    /home/spes/EPICS/base-7.0.8.1/support/modbus/dbd/modbusSupport.dbd \
+    /home/spes/EPICS/base-7.0.8.1/support/autosave/dbd/asSupport.dbd
+
+/home/spes/EPICS/base-7.0.8.1/dbd/base.dbd:
+/home/spes/EPICS/base-7.0.8.1/dbd/menuGlobal.dbd:
+/home/spes/EPICS/base-7.0.8.1/dbd/menuAlarmSevr.dbd:
+/home/spes/EPICS/base-7.0.8.1/dbd/menuAlarmStat.dbd:
+/home/spes/EPICS/base-7.0.8.1/dbd/menuFtype.dbd:
+/home/spes/EPICS/base-7.0.8.1/dbd/menuIvoa.dbd:
+/home/spes/EPICS/base-7.0.8.1/dbd/menuOmsl.dbd:
+/home/spes/EPICS/base-7.0.8.1/dbd/menuPini.dbd:
+/home/spes/EPICS/base-7.0.8.1/dbd/menuPost.dbd:
+/home/spes/EPICS/base-7.0.8.1/dbd/menuPriority.dbd:
+/home/spes/EPICS/base-7.0.8.1/dbd/menuYesNo.dbd:
+/home/spes/EPICS/base-7.0.8.1/dbd/menuSimm.dbd:
+/home/spes/EPICS/base-7.0.8.1/dbd/menuConvert.dbd:
+/home/spes/EPICS/base-7.0.8.1/dbd/menuScan.dbd:
+/home/spes/EPICS/base-7.0.8.1/dbd/stdRecords.dbd:
+/home/spes/EPICS/base-7.0.8.1/dbd/aaiRecord.dbd:
+/home/spes/EPICS/base-7.0.8.1/dbd/dbCommon.dbd:
+/home/spes/EPICS/base-7.0.8.1/dbd/aaoRecord.dbd:
+/home/spes/EPICS/base-7.0.8.1/dbd/aiRecord.dbd:
+/home/spes/EPICS/base-7.0.8.1/dbd/aoRecord.dbd:
+/home/spes/EPICS/base-7.0.8.1/dbd/aSubRecord.dbd:
+/home/spes/EPICS/base-7.0.8.1/dbd/biRecord.dbd:
+/home/spes/EPICS/base-7.0.8.1/dbd/boRecord.dbd:
+/home/spes/EPICS/base-7.0.8.1/dbd/calcRecord.dbd:
+/home/spes/EPICS/base-7.0.8.1/dbd/calcoutRecord.dbd:
+/home/spes/EPICS/base-7.0.8.1/dbd/compressRecord.dbd:
+/home/spes/EPICS/base-7.0.8.1/dbd/dfanoutRecord.dbd:
+/home/spes/EPICS/base-7.0.8.1/dbd/eventRecord.dbd:
+/home/spes/EPICS/base-7.0.8.1/dbd/fanoutRecord.dbd:
+/home/spes/EPICS/base-7.0.8.1/dbd/histogramRecord.dbd:
+/home/spes/EPICS/base-7.0.8.1/dbd/int64inRecord.dbd:
+/home/spes/EPICS/base-7.0.8.1/dbd/int64outRecord.dbd:
+/home/spes/EPICS/base-7.0.8.1/dbd/longinRecord.dbd:
+/home/spes/EPICS/base-7.0.8.1/dbd/longoutRecord.dbd:
+/home/spes/EPICS/base-7.0.8.1/dbd/lsiRecord.dbd:
+/home/spes/EPICS/base-7.0.8.1/dbd/lsoRecord.dbd:
+/home/spes/EPICS/base-7.0.8.1/dbd/mbbiRecord.dbd:
+/home/spes/EPICS/base-7.0.8.1/dbd/mbbiDirectRecord.dbd:
+/home/spes/EPICS/base-7.0.8.1/dbd/mbboRecord.dbd:
+/home/spes/EPICS/base-7.0.8.1/dbd/mbboDirectRecord.dbd:
+/home/spes/EPICS/base-7.0.8.1/dbd/permissiveRecord.dbd:
+/home/spes/EPICS/base-7.0.8.1/dbd/printfRecord.dbd:
+/home/spes/EPICS/base-7.0.8.1/dbd/selRecord.dbd:
+/home/spes/EPICS/base-7.0.8.1/dbd/seqRecord.dbd:
+/home/spes/EPICS/base-7.0.8.1/dbd/stateRecord.dbd:
+/home/spes/EPICS/base-7.0.8.1/dbd/stringinRecord.dbd:
+/home/spes/EPICS/base-7.0.8.1/dbd/stringoutRecord.dbd:
+/home/spes/EPICS/base-7.0.8.1/dbd/subRecord.dbd:
+/home/spes/EPICS/base-7.0.8.1/dbd/subArrayRecord.dbd:
+/home/spes/EPICS/base-7.0.8.1/dbd/waveformRecord.dbd:
+/home/spes/EPICS/base-7.0.8.1/dbd/filters.dbd:
+/home/spes/EPICS/base-7.0.8.1/dbd/links.dbd:
+/home/spes/EPICS/base-7.0.8.1/dbd/devSoft.dbd:
+/home/spes/EPICS/base-7.0.8.1/dbd/asSub.dbd:
+/home/spes/EPICS/base-7.0.8.1/dbd/dbCore.dbd:
+/home/spes/EPICS/base-7.0.8.1/dbd/rsrv.dbd:
+/home/spes/EPICS/base-7.0.8.1/support/asyn/dbd/asyn.dbd:
+/home/spes/EPICS/base-7.0.8.1/support/asyn/dbd/asynRecord.dbd:
+/home/spes/EPICS/base-7.0.8.1/support/asyn/dbd/devEpics.dbd:
+/home/spes/EPICS/base-7.0.8.1/support/asyn/dbd/devAsynOctet.dbd:
+/home/spes/EPICS/base-7.0.8.1/support/asyn/dbd/devAsynInt32.dbd:
+/home/spes/EPICS/base-7.0.8.1/support/asyn/dbd/devAsynXXXArray.dbd:
+/home/spes/EPICS/base-7.0.8.1/support/asyn/dbd/devAsynInt32TimeSeries.dbd:
+/home/spes/EPICS/base-7.0.8.1/support/asyn/dbd/devAsynUInt32Digital.dbd:
+/home/spes/EPICS/base-7.0.8.1/support/asyn/dbd/devAsynFloat64.dbd:
+/home/spes/EPICS/base-7.0.8.1/support/asyn/dbd/devAsynFloat64TimeSeries.dbd:
+/home/spes/EPICS/base-7.0.8.1/support/asyn/dbd/devAsynRecord.dbd:
+/home/spes/EPICS/base-7.0.8.1/support/asyn/dbd/devAsynOctetLs.dbd:
+/home/spes/EPICS/base-7.0.8.1/support/asyn/dbd/devAsynInt64.dbd:
+/home/spes/EPICS/base-7.0.8.1/support/asyn/dbd/devAsynInt64Array.dbd:
+/home/spes/EPICS/base-7.0.8.1/support/asyn/dbd/devAsynInt64TimeSeries.dbd:
+/home/spes/EPICS/base-7.0.8.1/support/asyn/dbd/devAsynInt64Misc.dbd:
+/home/spes/EPICS/base-7.0.8.1/support/asyn/dbd/drvAsynIPPort.dbd:
+/home/spes/EPICS/base-7.0.8.1/support/modbus/dbd/modbusSupport.dbd:
+/home/spes/EPICS/base-7.0.8.1/support/autosave/dbd/asSupport.dbd:
+../O.Common/iris.dbd: ../Makefile
---
+++ EPICS IOC/iris20250924/irisApp/src/O.linux-x86_64/iris_registerRecordDeviceDriver.d
@@ -0,0 +1,27 @@
+iris_registerRecordDeviceDriver.o: iris_registerRecordDeviceDriver.cpp \
+ /home/isolpharm_iris/EPICS/base-7.0.8.1/include/compilerDependencies.h \
+ /home/isolpharm_iris/EPICS/base-7.0.8.1/include/compiler/gcc/compilerSpecific.h \
+ /home/isolpharm_iris/EPICS/base-7.0.8.1/include/epicsStdlib.h \
+ /home/isolpharm_iris/EPICS/base-7.0.8.1/include/libComAPI.h \
+ /home/isolpharm_iris/EPICS/base-7.0.8.1/include/os/Linux/osdStrtod.h \
+ /home/isolpharm_iris/EPICS/base-7.0.8.1/include/epicsTypes.h \
+ /home/isolpharm_iris/EPICS/base-7.0.8.1/include/compilerDependencies.h \
+ /home/isolpharm_iris/EPICS/base-7.0.8.1/include/errMdef.h \
+ /home/isolpharm_iris/EPICS/base-7.0.8.1/include/iocsh.h \
+ /home/isolpharm_iris/EPICS/base-7.0.8.1/include/iocshRegisterCommon.h \
+ /home/isolpharm_iris/EPICS/base-7.0.8.1/include/dbCoreAPI.h \
+ /home/isolpharm_iris/EPICS/base-7.0.8.1/include/registryCommon.h \
+ /home/isolpharm_iris/EPICS/base-7.0.8.1/include/dbStaticLib.h \
+ /home/isolpharm_iris/EPICS/base-7.0.8.1/include/dbFldTypes.h \
+ /home/isolpharm_iris/EPICS/base-7.0.8.1/include/dbBase.h \
+ /home/isolpharm_iris/EPICS/base-7.0.8.1/include/ellLib.h \
+ /home/isolpharm_iris/EPICS/base-7.0.8.1/include/dbDefs.h \
+ /home/isolpharm_iris/EPICS/base-7.0.8.1/include/recSup.h \
+ /home/isolpharm_iris/EPICS/base-7.0.8.1/include/devSup.h \
+ /home/isolpharm_iris/EPICS/base-7.0.8.1/include/link.h \
+ /home/isolpharm_iris/EPICS/base-7.0.8.1/include/cantProceed.h \
+ /home/isolpharm_iris/EPICS/base-7.0.8.1/include/dbJLink.h \
+ /home/isolpharm_iris/EPICS/base-7.0.8.1/include/dbCoreAPI.h \
+ /home/isolpharm_iris/EPICS/base-7.0.8.1/include/registryRecordType.h \
+ /home/isolpharm_iris/EPICS/base-7.0.8.1/include/recSup.h \
+ /home/isolpharm_iris/EPICS/base-7.0.8.1/include/shareLib.h
